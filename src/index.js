a1a16b7a002f7b1d90265d64ff7f5ee95ffe565f
lgfh98 <lgfigueroah@outlook.com>
2021-08-18 16:13:09 -0500

    webpack.config.js basico para JS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import Template from './templates/Template.js';
-console.log('hola');
-
+import Template from "./templates/Template.js";
+console.log("hola mundo");
 (async function App() {
-  const main = null || document.getElementById('main');
+  const main = null || document.getElementById("main");
   main.innerHTML = await Template();
 })();
