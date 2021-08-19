750cd28b7c43b58d51ce5891d6f1004bf3f07402
lgfh98 <lgfigueroah@outlook.com>
2021-08-19 11:50:10 -0500

    archivo webpack.config.dev.js para modo desarrollo

---
+++ webpack.config.dev.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const DotEnv = require("dotenv-webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "assets/[name][contenthash].js",
+    assetModuleFilename: "assets/[hash][ext][query]",
+  },
+  mode: "development",
+  resolve: {
+    extensions: [".js"],
+    alias: {
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@fonts": path.resolve(__dirname, "src/assets/fonts"),
+      "@templates": path.resolve(__dirname, "src/templates"),
+      "@images": path.resolve(__dirname, "src/assets/images"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css|.scss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.resolve(__dirname, "public/index.html"),
+      filename: "index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "assets/[name].[contenthash].css",
+    }),
+    new DotEnv(),
+  ],
+};
